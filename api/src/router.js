5ef0e0dc4e5bc0cb16f53838ed1340d4127e4c87
Patrick Richardson <prichardson@conversantmedia.com>
2021-02-09 15:54:20 -0600

    added post route for file upload

---
+++ api/src/router.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const multer = require("multer");
+
+const router = Router();
+
+const filename = (request, file, callback) => callback(null, file.originalname);
+
+const storage = multer.diskStorage({
+  destination: "api/uploads/",
+  filename: filename,
+});
+
+const fileFilter = (request, file, callback) => {
+  if (file.mimetype !== "image/png") {
+    const err = "Wrong file type";
+    request.fileValidationError = err;
+    callback(null, false, new Error(err));
+  } else {
+    callback(null, true);
+  }
+};
+
+const upload = multer({ fileFilter, storage });
+
+router.post("./upload", upload.single("photo"), (request, response) => {
+  if (request.fileValidationError) {
+    return response.status(400).json({ error: request.fileValidationError });
+  }
+
+  return response.status(201).json({ success: true });
+});
+
+module.exports = router;
