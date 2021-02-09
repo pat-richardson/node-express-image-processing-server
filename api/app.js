dea0fcaa3f2e2fb771ebeca48702ade52fac42ac
Patrick Richardson <prichardson@conversantmedia.com>
2021-02-09 15:17:02 -0600

    Completed module

--- api/app.js
+++ api/app.js
@@ -4,7 +4,9 @@ const path = require("path");
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, "../client/index.html");
+
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
 });
-module.exports = { app };
+
+module.exports = app;
